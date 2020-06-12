a8383768f8844c9610fc8735efdb0745467e4c05
Prerak Raja <rajaprerak@gmail.com>
2020-06-12 10:23:11 +0530

    heroku deployment static error

--- movie_recommender/settings.py
+++ movie_recommender/settings.py
@@ -119,11 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = ()
+# STATICFILES_DIRS = ([
+#     os.path.join(BASE_DIR, "static"),
+# ])
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
